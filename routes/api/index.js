1b477caf72fa9519d89ef961a7d6b6df9456d972
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-01-14 18:15:42 -0600

    api/index.js

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoute = require('./userRoute');
+const thoughtRoute = require('./thoughtRoute');
+
+router.use('/thoughts', thoughtRoute);
+router.use('./users', userRoute);
+
+module.exports = router;
